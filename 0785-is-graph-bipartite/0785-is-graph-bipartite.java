641dfcc2a6d68b14b93255bb2a7c617a5d5ff3c6
Vaibhav Shendre <vaibhavshendre58@gmail.com>
2026-07-24 22:34:52 +0530

    Time: 1 ms (60.95%), Space: 47.3 MB (19.36%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,37 +1,36 @@
 class Solution {
     static boolean ans;
-    public void bfs(int i,int[][] graph,int[] visited){
+    public static void bfs(int i,int[][] graph, int[] visited){
         Queue<Integer> q = new LinkedList<>();
-        visited[i] = 0;
         q.add(i);
+        visited[i] = 0;
         while(q.size()>0){
-            int front = q.remove();
-            int color = visited[front];
-            for(int ele : graph[front]){
-
-            
-            if(visited[ele]==visited[front]){
+          int front = q.remove();
+          
+          int color = visited[front];
+          for(int ele : graph[front]){
+              if(visited[ele]==color){
                 ans = false;
                 return;
-            }
-            if(visited[ele]==-1){
+              }
+              if(visited[ele]==-1){
                 visited[ele] = 1-color;
                 q.add(ele);
-            }
-
-            }
+              }
+          }
         }
     }
     public boolean isBipartite(int[][] graph) {
         ans = true;
         int n = graph.length;
         int[] visited = new int[n];
-
         Arrays.fill(visited,-1);
 
         for(int i=0; i<n; i++){
             if(ans==false) return ans;
-            if(visited[i]==-1) bfs(i,graph,visited);
+            if(visited[i]==-1){
+                bfs(i,graph,visited);
+            }
         }
         return ans;
     }
